84bf20137d6a171459a3e0cd308111840c752826
Nika Kolesnikova <kolesnikova.ve.0@gmail.com>
2026-04-06 22:12:39 -0400

    feat: Implement a delete button and a delete functionality

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -6,6 +6,26 @@ const convertToDateString = (date: string) => {
     return new Date(date).toDateString();
 }
 
+
+const DeleteButton = ({ taskId }: { taskId: string }) => {
+    const { dispatch } = useTasks();
+
+    const handleDelete = (taskId: string) => {
+    // Implementation for delete functionality
+    if (window.confirm("Are you sure you want to delete this task?")) {
+        dispatch({ type: 'delete', payload: { id: taskId } });
+    }
+}
+
+    return (
+        <button 
+            className="cursor-pointer bg-red-950 hover:bg-amber-800 text-white px-2 py-1 rounded"
+            onClick={() => handleDelete(taskId)}>
+                Delete
+        </button>
+     );
+}
+
 export const TaskList: React.FC = () => {
 
     const { tasks } = useTasks();
@@ -23,7 +43,7 @@ export const TaskList: React.FC = () => {
                     Priority: task.priority,
                     "Created At": createdAt,
                     "Updated At": updatedAt,
-                    Button: null, // Placeholder for the buttons cell
+                    Button: <DeleteButton  taskId={task.id} />, // Placeholder for the buttons cell
                 }
             }
         })
